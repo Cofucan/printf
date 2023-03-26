6ff6a3c314e0091ce633ba95ce8983dd624da47e
cofucan <coolucheofia@gmail.com>
2023-03-26 23:13:06 +0100

    feat: Function to print a float to stdout

---
+++ print_float.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int print_float(float num)
+{
+	char sign = '-';
+	char dot = '.';
+	int bytes = 0;
+	char digit;
+	int integer_part;
+	float fractional_part;
+
+	if (num < 0)
+	{
+		bytes += write(1, &sign, sizeof(char));
+		num = -num;
+	}
+
+	integer_part = (int)num;
+	fractional_part = num - integer_part;
+
+	/* Print integer part and count bytes printed */
+	bytes += print_int(integer_part);
+
+	/* Print the dot sign between the integer and fractional parts */
+	bytes += write(1, &dot, sizeof(char));
+
+	/* Print fractional part and count the bytes printed */
+	while (fractional_part)
+	{
+		fractional_part *= 10;
+		digit = (int)fractional_part + '0';
+		bytes += write(1, &digit, sizeof(char));
+		fractional_part -= digit;
+	}
+
+	return (bytes);
+}
