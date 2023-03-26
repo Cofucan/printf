9990acf5dc365a0e05bcb6e1678ac16f90d99816
cofucan <coolucheofia@gmail.com>
2023-03-26 23:23:31 +0100

    style: Betty compliant

--- util-no_of_digits.c
+++ util-no_of_digits.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * number_of_digits - returns the number of digits in an integer
+ * @num: The number to be processed
+ *
+ * Return: The number of digits in that number.
+ */
+
 int number_of_digits(int num)
 {
 	int x = num;
